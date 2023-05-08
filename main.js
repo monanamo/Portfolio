db0f5d410442b35015adceafcacbac9a1840e8af
monanamo <130433811+monanamo@users.noreply.github.com>
2023-05-08 21:32:24 +0900

    JS: arrow-up

--- main.js
+++ main.js
@@ -42,6 +42,22 @@ document.addEventListener('scroll', ()=> {
 });
 
 
+// Show "arrow up" button when scrolling down
+const arrowUp = document.querySelector('.arrow-up');
+document.addEventListener('scroll', ()=> {
+    if(window.scrollY > homeHeight /2) {
+        arrowUp.classList.add('visible');
+    } else {
+        arrowUp.classList.remove('visible');
+    }
+});
+
+
+// Handle click on the "arrow up" button
+arrowUp.addEventListener('click', ()=> {
+    scrollIntoView('.home');
+});
+
 function scrollIntoView (selector) {
     const scrollTo = document.querySelector('#contact');
     scrollTo.scrollIntoView({behavior: 'smooth'});
