5f2bfd409c8e9a11415d063c1cf0bf2c20dd1113
monanamo <130433811+monanamo@users.noreply.github.com>
2023-05-11 11:05:50 +0900

    JS: error edit

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 'use strict';
 
+// Make navbar transparent when it is on the top
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
-    console.log(window.scrollY);
-    console.log('navbarHeight: ${navbarHeight');
-    if(window.scrollY > navbarHeight) {
+    if (window.scrollY > navbarHeight) {
         navbar.classList.add('navbar--dark');
     } else {
         navbar.classList.remove('navbar--dark');
@@ -15,16 +14,19 @@ document.addEventListener('scroll', () => {
 // Handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event) => {
-    console.log(event.target.dataset.link);
     const target = event.target;
     const link = target.dataset.link;
     if (link == null) {
         return;
     }
+    navbarMenu.classList.remove('opne');
+    scrollIntoView(link);
+});
 
-    console.log(event.target.dataset.link);
-    const scrollTo = document.querySelector(link);
-    scrollTo.scrollIntoView({behavior: 'smooth'});
+// Navbar toggle button for small screen
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+navbarToggleBtn.addEventListener('click', () => {
+    navbarMenu.classList.toggle('open');
 });
 
 // Handle click on "contact me" button on home
@@ -33,19 +35,17 @@ homeContactBtn.addEventListener('click', () => {
     scrollIntoView('#contact');
 });
 
-
 // Make home slowly fade to transparent as the window scrolls down
-const home = document.querySelector('#home');
+const home = document.querySelector('.home__container');
 const homeHeight = home.getBoundingClientRect().height;
-document.addEventListener('scroll', ()=> {
+document.addEventListener('scroll', () => {
     home.style.opacity = 1 - window.scrollY / homeHeight;
 });
-
 
 // Show "arrow up" button when scrolling down
 const arrowUp = document.querySelector('.arrow-up');
 document.addEventListener('scroll', ()=> {
-    if(window.scrollY > homeHeight /2) {
+    if(window.scrollY > homeHeight / 2) {
         arrowUp.classList.add('visible');
     } else {
         arrowUp.classList.remove('visible');
@@ -53,32 +53,28 @@ document.addEventListener('scroll', ()=> {
 });
 
 
-// Handle click on the "arrow up" button
-arrowUp.addEventListener('click', ()=> {
-    scrollIntoView('#home');
-});
-
-
 // Portfolio
 const workBtnContainer = document.querySelector('.work__categories');
 const projectContainer = document.querySelector('.work__projects');
 const projects = document.querySelectorAll('.project');
-workBtnContainer.addEventListener('click', (e)=> {
-    const filter = e.target.dataset.filter || e.target.prentNode.dataset.filter;
-    if(filter == null) {
+workBtnContainer.addEventListener('click', (e) => {
+    const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+    if (filter == null) {
         return;
     }
-    
+
     // Remove selection from the previous item and select the new one
     const active = document.querySelector('.category__btn.selected');
-    active.classList.remove('selected');
+    if (active != null) {
+        active.classList.remove('selected');
+    }
     e.target.classList.add('selected');
 
     projectContainer.classList.add('anim-out');
     setTimeout(() => {
         projects.forEach((project) => {
-            console.log(project.dataset.type);
-            if(filter ==='*' || filter === project.dataset.type) {
+        console.log(project.dataset.type);
+        if (filter === '*' || filter === project.dataset.type) {
                 project.classList.remove('invisible');
             } else {
                 project.classList.add('invisible');
@@ -86,9 +82,12 @@ workBtnContainer.addEventListener('click', (e)=> {
         });
         projectContainer.classList.remove('anim-out');
     }, 300);
-
 });
 
+    function scrollIntoView(selector) {
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({ behavior: 'smooth' });
+    }
 function scrollIntoView (selector) {
     const scrollTo = document.querySelector('#contact');
     scrollTo.scrollIntoView({behavior: 'smooth'});
